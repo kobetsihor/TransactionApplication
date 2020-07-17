e3ec05f4f38617069c862da34a19892bc30a3178
Ihor Kobets <kobets.ihor@gmail.com>
2020-07-17 18:06:19 +0300

    Added tests, updated .gitignore

---
+++ TransactionApplication.Tests/API/Controllers/TransactionsControllerTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using AutoFixture;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using NUnit.Framework;
+using TransactionApplication.API.Abstractions;
+using TransactionApplication.API.Controllers;
+using TransactionApplication.BL.Abstractions;
+using TransactionApplication.BL.Models;
+
+namespace TransactionApplication.Tests.API.Controllers
+{
+    public class TransactionsControllerTests
+    {
+        private ITransactionsController _transactionsController;
+        private Mock<ITransactionService> _transactionServiceMock;
+        private Fixture _fixture;
+
+        [SetUp]
+        public void Init()
+        {
+            _fixture = new Fixture();
+            _transactionServiceMock = new Mock<ITransactionService>();
+            _transactionsController = new TransactionsController(_transactionServiceMock.Object);
+        }
+
+        [Test]
+        public async Task GetAllTransactionsShouldReturnSuccessResult()
+        {
+            var transactions = _fixture.CreateMany<TransactionModel>();
+
+            _transactionServiceMock.Setup(x => x.GetAllTransactions()).ReturnsAsync(transactions);
+
+            var response = await _transactionsController.GetAllTransactions() as OkObjectResult;
+            var data = response?.Value as IEnumerable<TransactionModel>;
+
+            Assert.IsNotNull(response);
+            Assert.AreEqual(transactions, data);
+            _transactionServiceMock.Verify(x => x.GetAllTransactions(), Times.Once);
+        }
+    }
+}
---
+++ TransactionApplication.Tests/BL/Services/TransactionServiceTests.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using AutoFixture;
+using AutoMapper;
+using Moq;
+using NUnit.Framework;
+using TransactionApplication.BL.Abstractions;
+using TransactionApplication.BL.Models;
+using TransactionApplication.BL.Services;
+using TransactionApplication.DAL.Entities;
+using TransactionApplication.DAL.Repositories;
+
+namespace TransactionApplication.Tests.BL.Services
+{
+    public class TransactionServiceTests
+    {
+        private Fixture _fixture;
+        private ITransactionService _transactionsService;
+        private Mock<ITransactionDataParser> _transactionsDataParser;
+        private Mock<ITransactionParserProvider> _transactionsParserProviderMock;
+        private Mock<ITransactionRepository> _transactionRepositoryMock;
+        private Mock<IMapper> _mapperMock;
+
+        [SetUp]
+        public void Init()
+        {
+            _fixture = new Fixture();
+            _mapperMock = new Mock<IMapper>();
+            _transactionRepositoryMock = new Mock<ITransactionRepository>();
+            _transactionsParserProviderMock = new Mock<ITransactionParserProvider>();
+            _transactionsDataParser = new Mock<ITransactionDataParser>();
+            _transactionsService = new TransactionService(_transactionsParserProviderMock.Object,
+                                                          _transactionRepositoryMock.Object,
+                                                          _mapperMock.Object);
+        }
+
+        [Test]
+        public async Task GetFilteredShouldReturnModels()
+        {
+            var status = "status";
+            var currencyCode = "currencyCode";
+            var entities = _fixture.CreateMany<Transaction>();
+            var models = _fixture.CreateMany<TransactionModel>();
+
+            _transactionRepositoryMock.Setup(x => x.GetFiltered(currencyCode, null, null, status))
+                                                                            .ReturnsAsync(entities);
+
+            _mapperMock.Setup(x => x.Map<IEnumerable<TransactionModel>>(entities)).Returns(models);
+
+            var result = await _transactionsService.GetFilteredTransactions(currencyCode, null, null, status);
+          
+            Assert.AreEqual(models, result);
+            _transactionRepositoryMock.Verify(x => x.GetFiltered(currencyCode, null, null, status), Times.Once);
+            _mapperMock.Verify(x => x.Map<IEnumerable<TransactionModel>>(entities), Times.Once);
+        }
+
+        [Test]
+        public async Task AddTransactionsShouldBeHandled()
+        {
+            const string csvString = @"“Invoice777”,”600.00”,”EUR”,”10/17/2019 02:04:59”, “Done”";
+            var csvDataStream = new MemoryStream(Encoding.UTF8.GetBytes(csvString));
+            const string csvDatePattern = "dd/MM/yyyy hh:mm:ss";
+
+            var publicId = "Invoice777";
+            var code = "EUR";
+            var status = "D";
+            var amount = 600;
+            var date = DateTimeOffset.ParseExact("10/17/2019 02:04:59", csvDatePattern, CultureInfo.InvariantCulture);
+
+            var entity = new Transaction
+            {
+                PublicId = publicId,
+                Amount = amount,
+                Date = date,
+                Code = code,
+                Status = status
+            };
+
+            var model = new TransactionCreateModel
+            {
+                PublicId = publicId,
+                Amount = amount,
+                Date = date,
+                Code = code,
+                Status = status
+            };
+
+            var entities = new List<Transaction> { entity };
+            var models = new List<TransactionCreateModel> { model };
+            var parseResults = new ParseResults(models);
+
+            _transactionsParserProviderMock.Setup(x => x.GetParser(csvDataStream)).ReturnsAsync(_transactionsDataParser.Object);
+            _transactionsDataParser.Setup(x => x.CanParseData(csvDataStream)).ReturnsAsync(true);
+            _transactionsDataParser.Setup(x => x.ParseAllFile()).ReturnsAsync(parseResults);
+            _mapperMock.Setup(x => x.Map<IEnumerable<Transaction>>(models)).Returns(entities);
+            _transactionRepositoryMock.Setup(x => x.Add(entity));
+
+            await _transactionsService.AddTransactions(csvDataStream);
+
+            _transactionsParserProviderMock.Verify(x => x.GetParser(csvDataStream), Times.Once);
+            _transactionsDataParser.Verify(x => x.CanParseData(csvDataStream), Times.Once);
+            _transactionsDataParser.Verify(x => x.ParseAllFile(), Times.Once);
+            _mapperMock.Verify(x => x.Map<IEnumerable<Transaction>>(models), Times.Once);
+            _transactionRepositoryMock.Verify(x => x.Add(entity), Times.Once);
+        }
+    }
+}
